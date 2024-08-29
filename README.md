9d9417309a1a1e3203ad645f5dc549761f25a86f
muad_nuri <muadalmaki@gmail.com>
2024-08-29 17:20:21 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## library_mgm
+
+nn
+
+#### License
+
+MIT
---
+++ library_management/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "library_mgm",
+			"type": "module",
+			"label": _("library_mgm")
+		}
+	]
